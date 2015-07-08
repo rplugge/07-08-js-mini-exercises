9b26831e07a76009b639c069755ee5a5aedd8d2a
Sam Morgan <samorgan@huskers.unl.edu>
2015-07-08 13:52:31 -0500

    Completed auto tab phone number

--- AutoTabPhoneNumber/global.js
+++ AutoTabPhoneNumber/global.js
@@ -1,27 +1,9 @@
-var tab1content = "Tab 1 Tab 1 Tab 1 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
-var tab2content = "Tab 2 Tab 2 Tab 2 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-
-var tab3content = "Tab 3 Tab 3 Tab 3 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-
-
-var a = tab1content;
-
-
-var b = tab2content;
-
-var c = tab3content;
-
-function changeContent(id) {
-  if (id == "Tab1") {
-    document.getElementById("tabcontent").innerText = a;
+function autoTab(current, next){
+  if (current.getAttribute &&
+    current.value.length == current.getAttribute("maxlength")){
+    next.focus()
   }
-  if (id == "Tab2") {
-    document.getElementById("tabcontent").innerText = b;
-  }
-  if (id == "Tab3") {
-    document.getElementById("tabcontent").innerText = c;
-  }
+  
+  
 }
-
-document.getElementById("tabcontent").innerText = a;
