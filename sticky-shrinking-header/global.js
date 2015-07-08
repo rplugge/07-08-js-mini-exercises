2c239ce8616198322ab33c3b37c5eb637a22133d
Sam Morgan <samorgan@huskers.unl.edu>
2015-07-08 15:50:45 -0500

    got onscroll to work

--- sticky-shrinking-header/global.js
+++ sticky-shrinking-header/global.js
@@ -1,6 +1,11 @@
-var resize_header = function() {
-  if (window.pageYOffset > 250) {
-    document.getElementById("header").style.height = "50px";
-    console.log("Pooper")
-  }
+var d = document.getElementById("header");
+
+
+
+function scrollFunction() {
+  d.className = d.className + " tiny";
 }
+
+
+
+window.onscroll = scrollFunction;
